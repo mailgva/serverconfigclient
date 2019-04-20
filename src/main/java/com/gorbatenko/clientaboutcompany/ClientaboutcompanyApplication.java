a5315fcd146d69b9b04c1457dab914b01e2227f6
Gorbatenko Vladimir <mail.gva@gmail.com>
2019-04-20 17:59:43 +0300

    first commit

---
+++ src/main/java/com/gorbatenko/clientaboutcompany/ClientaboutcompanyApplication.java
@@ -0,0 +1,13 @@
+package com.gorbatenko.clientaboutcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientaboutcompanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientaboutcompanyApplication.class, args);
+    }
+
+}
